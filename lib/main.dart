3ee9bb3d0784f42a214983875284e3ae7620ec79
Matthias Mulugeta <matthias22m@gmail.com>
2024-05-30 12:40:46 +0300

    modified: add router to home

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rtc_app/auth/loginorregister.dart';
+import 'package:rtc_app/pages/home.dart';
 import 'package:rtc_app/pages/login.dart';
 import 'package:rtc_app/pages/register.dart';
 
@@ -19,6 +20,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
      ),
       home: Loginorregister(),
+      routes: {
+        '/home' : (context)=> MyHomePage(),
+      },
     );
   }
 }
